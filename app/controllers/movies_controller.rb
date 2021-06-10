bded2d3be8092020dd8d48299f2db8311588bdda
NaotoTajima <tajima701025@gmail.com>
2021-06-10 11:07:39 +0000

    TMDB search finish

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,4 @@
 class MoviesController < ApplicationController
-  # TMDBからAPIkeyとgemを指定するためのに下記を記載
   require 'themoviedb-api'
   Tmdb::Api.key("6202f5cb38f1e6aafa8bd900346a9106")
   Tmdb::Api.language("ja")
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'movies/search'
-  get 'movies/show'
   devise_for :users
   root to: "homes#top"
+
+  get 'movies/search'
+  get 'movies/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
