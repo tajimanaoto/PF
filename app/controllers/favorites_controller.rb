e0bdc6bcb859bc818329f5d51bf9d567d282733e
NaotoTajima <tajima701025@gmail.com>
2021-06-17 00:47:46 +0000

    Update favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,12 @@ class FavoritesController < ApplicationController
     @tweet = Tweet.find(params[:tweet_id])
     favorite = current_user.favorites.new(tweet_id: @tweet.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
     @tweet = Tweet.find(params[:tweet_id])
     favorite = current_user.favorites.find_by(tweet_id: @tweet.id)
     favorite.destroy
-    redirect_to request.referer
   end
 
 end
