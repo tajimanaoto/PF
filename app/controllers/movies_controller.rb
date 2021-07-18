653727d4e105017ced9558a53152e9043a42b5db
NaotoTajima <tajima701025@gmail.com>
2021-07-18 10:50:25 +0000

    Update TMDB API

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,8 @@ class MoviesController < ApplicationController
  #rails cを使用しコンソール上でTmdb::Api.key("6202f5cb38f1e6aafa8bd900346a9106")
  # Tmdb::Api.language("ja")を打ち込んで一度読み込ませる必要がある
   require 'themoviedb-api'
-  Tmdb::Api.key("6202f5cb38f1e6aafa8bd900346a9106")
+
+  Tmdb::Api.key(ENV["TMDB_API_KEY"])
   Tmdb::Api.language("ja")
 
   def search
